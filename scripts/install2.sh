7c268a0efcd33e44d308289b48b9067d8e0bbe5b
Jeremy <jclifton44@gmail.com>
2017-06-21 10:37:56 -0500

    scripts folder

--- scripts/install2.sh
+++
@@ -1,8 +0,0 @@
-mount /dev/sda2 /boot
-##bootctl --path=/boot install
-
-cp loader.conf /boot/loader/loader.conf
-uuid=$(./getUUID.sh)
-type=$(./getTYPE.sh)
-cp /mnt/arch.conf /boot/loader/entries/arch.conf
-echo "options root=PARTUUID=${uuid} rw" >> /boot/loader/entries/arch.conf
