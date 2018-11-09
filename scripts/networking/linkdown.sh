609185b3504a837c701d5f68c1f57dc9c593cc1b
Jeremy Clifton <jeremyclif@outlook.com>
2018-11-09 16:44:16 -0500

    disconnect from AP

--- scripts/networking/linkdown.sh
+++ scripts/networking/linkdown.sh
@@ -2,6 +2,7 @@ echo "Before Link Down"
 pwd=$(pwd)
 sh "$pwd/scripts/networking/status.sh"
 ip link set wlp2s0 down
+killall wpa_supplicant
 echo "After Link Down"
 sh "$pwd/scripts/networking/status.sh"
 
