5e5f26b29f22e601c80a632bbf89342556324c91
Jeremy Clifton <jeremyclif@outlook.com>
2018-11-10 12:33:14 -0500

    createArchIso.sh

--- scripts/createArchIso.sh
+++ scripts/createArchIso.sh
@@ -2,15 +2,17 @@
 ./sGit.sh git://projects.archlinux.org/archiso.git
 cd archiso
 make install
+cd ../
 #rm -rf archiso
+rm -rf ~/liveInstall
 mkdir ~/liveInstall
 cp -r /usr/share/archiso/configs/releng/*  ~/liveInstall
-mkdir ~/liveInstall/installScripts
-cp -r ../* ~/liveInstall/installScripts
+mkdir ~/liveInstall/airootfs/root/installScripts
+cp -r ../* ~/liveInstall/airootfs/root/installScripts
 #cd ~/liveInstall
-cd networking
+cd networking/
 ./startWPA2Wireless.sh
-#cd ../
+cd ../
 dhcpcd
 sleep 3
 echo "ip lease"
@@ -18,7 +20,7 @@ savedDirectory=$(pwd)
 networking/tableOff.sh
 networking/linkup.sh
 cd ~/liveInstall
-build.sh -v
+./build.sh -v
 cd $savedDirectory
 networking/tableOn.sh
 networking/linkdown.sh
