d0154c2f6f58bb57959ad3f9a21a17a202d4c84f
Jeremy Clifton <jeremyclif@outlook.com>
2018-11-11 17:24:14 -0500

    burnarchiso.sh

--- scripts/burnArchIso.sh
+++ scripts/burnArchIso.sh
@@ -1,25 +1,32 @@
 curl -C - -o ver https://mirrors.acm.wpi.edu/archlinux/iso/latest/md5sum.txt
 version=$(cat ver | grep linux-2 | sed -s 's/.*archlinux-//g' | sed -s 's/-.*//g')
 echo "Latest Version: $version"
-if [ $1 == "-c" ];
+if [ $2 == "-c" ];
 then
 	#make custom iso, burn it.
+	./createArchIso.sh 
+        dd bs=4M if=archCustom.iso of=$1 status=progress oflag=sync
 else
 	if [ ! -e archlinux.*.iso ];
 	then
 		curl -C - -o archlinux-$version-x86_64.iso https://mirrors.acm.wpi.edu/archlinux/iso/$version/archlinux-$version-x86_64.iso
 		curl -C - -o archlinux-$version-x86_64.iso.sig https://mirrors.acm.wpi.edu/archlinux/iso/$version/archlinux-$version-x86_64.iso.sig
-		if [ $? -eq 0 ];
-		then
-			echo "GPG - Gnu Privacy Gaurd Verified"
-		else
-			echo "GPG - Not Verifiable"
-			echo "Exiting USB imaging tool"
-			exit
-		fi
+		gpg --keyserver-options auto-key-retrieve --verify archlinux-$version-x86_64.iso.sig
 	else
 		version=$(ls -la | grep archlinux.*x86_64.iso$ | sed -s 's/.*archlinux-//g' | sed -s 's/-.*$//g')
 		curl -C - -o archlinux-$version-x86_64.iso.sig https://mirrors.acm.wpi.edu/archlinux/iso/$version/archlinux-$version-x86_64.iso.sig
 		gpg --keyserver-options auto-key-retrieve --verify archlinux-$version-x86_64.iso.sig
 	fi
+	if [ $? -eq 0 ];
+	then
+		echo "GPG - Gnu Privacy Gaurd Verified"
+		echo "Creating USB bootable"
+		dd bs=4M if=archlinux-2018.10.01-x86_64.iso of=$1 status=progress oflag=sync
+
+	else
+		echo "GPG - Not Verifiable"
+		echo "Exiting USB imaging tool"
+		exit
+	fi
+
 fi
--- scripts/createArchIso.sh
+++ scripts/createArchIso.sh
@@ -3,7 +3,7 @@
 cd archiso
 make install
 cd ../
-#rm -rf archiso
+rm -rf archiso
 rm -rf ~/liveInstall
 mkdir ~/liveInstall
 cp -r /usr/share/archiso/configs/releng/*  ~/liveInstall
@@ -21,6 +21,9 @@ networking/tableOff.sh
 networking/linkup.sh
 cd ~/liveInstall
 ./build.sh -v
+isoName=$(ls out/)
+echo "Moving new iso: $isoName"
+cp out/$isoName $savedDirectory/archCustom.iso
 cd $savedDirectory
 networking/tableOn.sh
 networking/linkdown.sh
