4a4702e2e68875a86c90caf8812ab5c9f1e2cff1
Jeremy Clifton <jeremyclif@outlook.com>
2018-11-17 10:07:00 -0500

    getRandomMirror bug

--- scripts/getRandomMirror.sh
+++ scripts/getRandomMirror.sh
@@ -1,5 +1,5 @@
-fileSize=$(wc -l /mnt/mirrors | sed "s/\s.*//")
-randLine=$((RANDOM % $fileSize))
-randServer=$(sed "${randLine}q;d" /mnt/mirrors)
+fileSize=$(wc -l ../mirrors | sed "s/\s.*//")
+randLine=$(($RANDOM % $fileSize))
+randServer=$(sed "${randLine}q;d" ../mirrors)
 url=$(sed "s/^#Server = //" <<< ${randServer})
 echo $url
