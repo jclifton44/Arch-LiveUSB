b83fa91257d9eb664c0368be4a5287a97d313f73
Jeremy Clifton <jeremyclif@outlook.com>
2018-11-20 13:39:54 -0500

    changed compress Install

--- shell/compressInstall.sh
+++ shell/compressInstall.sh
@@ -5,10 +5,11 @@ mkdir conf
 mkdir shell
 while read line
 do
-	cp /usr/bin/$line.sh shell/
+	cp /usr/bin/$line shell/$line.sh
+	
 done < /etc/shell/installedFiles
 cp -r /etc/shell/* conf
 rm -rf conf/installedFiles
 cd ../
-tar -cfv shell-scripts-source
+tar -zcvf compressed-shell-scripts-source.tar.gz $name
 rm -rf $name
