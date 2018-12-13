91ab6d2a7b2439d5e5ee2522d8ee26ed7c6ada25
root <root@instant0937.server.com>
2018-12-13 23:32:41 +0200

    configureServer

--- shell/remote/configureServer.sh
+++ shell/remote/configureServer.sh
@@ -2,10 +2,10 @@ apt-get update
 apt-get install nginx git python3
 curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
+rm get-pip.py
 pip install Flask
 ufw allow 'Nginx HTTP'
 ufw allow 'OpenSHH'
-ufw allow from 127.0.0.1 to 127.0.0.1 port 5000 proto tcp
 ufw allow ssh
 ufw app list
 ufw enable
