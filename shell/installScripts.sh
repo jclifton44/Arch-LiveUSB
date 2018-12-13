bd111b18fb2a676b27aa0c8da91b036873f29cb0
root <root@instant0937.server.com>
2018-12-14 00:43:15 +0200

    installscripts

--- shell/installScripts.sh
+++ shell/installScripts.sh
@@ -1,4 +1,6 @@
 cp -r ../conf/ /etc/shell
+mkdir /opt
+cp -r ../flask/ /opt/flask
 for line in $(ls); do 
 	cp $line /usr/bin/$(echo $line | sed -s 's/\.sh//g') 
 	echo $line | sed -s 's/\.sh//g' >> /etc/shell/installedFiles
