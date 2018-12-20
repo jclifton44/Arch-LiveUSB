37186c985077f79d7385d84074c24d3dfc192f26
Jeremy Clifton <jeremyclif@outlook.com>
2018-12-20 09:11:05 +0000

    conf folder

--- shell/installScripts.sh
+++ shell/installScripts.sh
@@ -2,7 +2,7 @@ mkdir /opt
 mkdir /opt/flask
 mkdir /etc/shell
 cp -r ../flask/ /opt
-cp -r ../conf/ /etc/shell
+cp -r ../conf/* /etc/shell
 for line in $(ls); do 
 	cp $line /usr/bin/$(echo $line | sed -s 's/\.sh//g') 
 	echo $line | sed -s 's/\.sh//g' >> /etc/shell/installedFiles
