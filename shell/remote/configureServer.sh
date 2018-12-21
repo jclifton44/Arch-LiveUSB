a642d0c141c1fab87f4a654f879380e457bf3d2d
root <root@jeremy-clifton.com>
2018-12-21 20:13:31 +0000

    ssl install

--- shell/remote/configureServer.sh
+++ shell/remote/configureServer.sh
@@ -1,6 +1,8 @@
 ovpn="false"
 flask="false"
 postfix="false"
+ssl="false"
+keyName="jeremy-clifton"
 for i in "$@"
 do	
 	case $i in
@@ -17,22 +19,53 @@ do
 		-p)
 			echo "-p set"
 			postfix="true"
+		;;
+		-ssl)
+			echo "ssl set"
+			ssl="true"
+			selfSign="true"
+		;;
+		-ca)
+			selfSign="false"
+
 	esac
 done
 apt-get update
 apt-get install nginx git
-ufw allow 'Nginx HTTP'
 ufw allow 'OpenSHH'
+ufw allow 'Nginx HTTP'
+cp /etc/shell/nginx.conf /etc/nginx/sites-enabled/default
+if [[ $ssl == 'true' ]];
+then
+	sed -i "s/#listen 443 ssl default_server;/listen 443 ssl default_server;/g" /etc/nginx/sites-enabled/default
+ 	sed -i "s/#listen [::]:443 ssl default_server;/listen [::]:443 ssl default_server;/g" /etc/nginx/sites-enabled/default
+	sed -i "s/#include snippets/ssl-params.conf;/include snippets/ssl-params.conf;/g" /etc/nginx/sites-enabled/default
+	ufw allow 'Nginx HTTPS'
+	openssl 	
+	cp /etc/shell/nginxSSL.conf /etc/nginx/snippets/ssl-params.conf
+	openssl dhparam -out /etc/ssl/dhparam.pem 2048
+	key-csr-cert $keyName -s
+	cp $keyName.key /etc/ssl/private/$keyName.key
+	cp $keyName.crt /etc/ssl/certs/$keyName.crt
+	rm $keyName.key
+	rm $keyName.crt
+	if [ $selfSign == 'false' ];
+	then
+		key-csr-cert $keyName
+		cp $keyName.key $HOME/$keyName.key
+		cp $keyName.csr $HOME/$keyName.csr
+		cp /etc/shell/CSRMessage $HOME/CSRMessage
+	fi
+fi
 ufw allow ssh
 ufw app list
 ufw enable
 ufw status verbose
-cp /etc/shell/nginx.conf /etc/nginx/sites-enabled/default
-cp /etc/shell/nginxSSL.conf /etc/nginx/snippets/ssl-params.conf
 systemctl restart nginx
 compressInstall
 mkdir /var/www/html/hosted
 cp compressed-shell-scripts-source.tar.gz /var/www/html/hosted/
+rm compressed-shell-scripts-source.tar.gz
 if [[ $ovpn == 'true' ]]; 
 then	
 	setupOVPN 
@@ -49,4 +82,3 @@ then
 	sleep 2
 	setupPostfix
 fi
-
