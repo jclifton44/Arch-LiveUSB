380eb03e804203d16578b5a1cd66ab6315724dc2
root <root@jeremy-clifton.com>
2018-12-22 17:38:11 +0000

    historyCommit

--- flask/historyDocumentation.py
+++ flask/historyDocumentation.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, request
-import logging
+import logging, pymongo, datetime, json
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
@@ -8,14 +10,32 @@ def hello():
 	return "Page History Versioning Utility"
 
 
-@app.route("/history/pageVisit/<commit>",methods=['GET','POST'])
+@app.route("/history/pageVisit/<commit>/",methods=['GET','POST'])
 def pageVisit(commit):
-	app.logger.info("IFNO MESSAGE:");
+	print('Connecting to client')
+	client=MongoClient()
+	db=client.historyDocumentation
+	visitedSites=db.visitedSites	
+	formData=request.form
+	#print formData['site']
+	responsePage="<h1>"+commit+": </h1> <br>Sites: <br>"
 	if request.method == "GET":
-		return request.data
+		print "GET MESSAGE"
+		pageVisitsDuringCommit=visitedSites.find({"commit":commit})	
+		for pageVisit in pageVisitsDuringCommit:
+			responsePage+=pageVisit['site']+"<br>"
+		
 	if request.method == "POST":	
-		return request.data	
-	return  "good"
+		siteVisit={"date": datetime.datetime.utcnow(),
+			"site":	formData['site'],
+			"commit": commit,
+			}
+		visitedSite_id=visitedSites.insert_one(siteVisit).inserted_id
+		#print "Inserted document id: " + visitedSite_id
+		
+	
+	client.close()
+	return  responsePage
 
 @app.route("/history/writeCommit/<commit>",methods=['GET','POST'])
 def writeCommit(commit):
