cfcff4c95f336fcfae345ad791142a673181e1a3
Jeremy Clifton <jeremyclif@outlook.com>
2018-12-31 05:51:48 -0500

    key ring

---
+++ shell/keyRing.sh
@@ -0,0 +1,62 @@
+
+
+listFlag="false"
+addFlag="false"
+removeFlag="false"
+touch /etc/shell/keyRingList
+mkdir -p /etc/shell/keys
+for i in "$@"
+do	
+	case $1 in
+		--list*)
+			echo "Key list:"
+			listFlag="true"
+			shift
+			break
+		;;
+		--add)
+			echo "Key add:"
+			addFlag="true"
+			keyName="$2"
+			shift
+			break
+		;;
+		--remove)
+			echo "Key remove:"
+			removeFlag="true"
+			shift
+			break
+	esac
+done
+if [[ $listFlag == "true" ]];
+then
+	cat /etc/shell/keyRingList
+
+fi
+
+if [[ $addFlag == "true" ]];
+then
+	cat /etc/shell/keyRingList | grep $keyName >> /dev/null
+	if [[ $? -ne 0 ]];
+	then
+		echo sendable | openssl s_client -servername $keyName -connect $keyName:443 >> /etc/shell/keys/$(echo $keyName | sed "s/\..*$//g").crt
+		echo $keyName >> /etc/shell/keyRingList
+	else
+		echo "Key already in key ring. Exiting."
+	fi
+
+
+fi
+
+if [[ $removeFlag == "true" ]];
+then
+	cat /etc/shell/keyRingList | grep $keyName >> /dev/null
+	if [[ $? -ne 0 ]];
+	then
+		echo "Key not in key ring. Exiting."
+	else
+		sed -i "s/$keyName//g" /etc/shell/keyRingList
+		rm /etc/shell/keys/$(echo $keyName | sed "s/\..*$//g").crt
+	fi
+
+fi
