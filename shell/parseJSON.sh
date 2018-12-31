88b63038086f7faf503d838447d670e196297ca0
Jeremy Clifton <jeremyclif@outlook.com>
2018-12-31 18:36:58 -0500

    parseJSON.sh

--- shell/parseJSON.sh
+++ shell/parseJSON.sh
@@ -1,5 +1,9 @@
-echo $1
-echo $1 | grep "^\s*{" | grep "}\s*$" 
+echo $@
+if [ -z $2 ]; then
+	echo $1
+	exit
+fi
+echo $1 | grep "^\s*{" | grep "}\s*$"  >> /dev/null
 if [ $? -eq 0 ];
 then
 	jsonObjectList=$(echo $1 | sed "s/^\s*{//g" | sed "s/}\s*$//")
@@ -31,20 +35,117 @@ then
 
 
 		#echo $kvPair
-		#key=$(echo $kvPair | sed "s/:.*$//g")
-		#value=$(echo $kvPair | sed "s/^[^:]*://g")
+		key=$(echo $kvPair | sed "s/:.*$//g")
+		value=$(echo $kvPair | sed "s/^[^:]*://g")
 		#echo "KEY: $key"
 		#echo "VALUE: $value"
+		if [ $2 == $key ];
+		then	
+			shift
+			shift
+			arguments=($value ${@[@]})
+			./parseJSON.sh $arguments
+		fi
 	done
 else
 	echo $1 | grep "^\s*\[" | grep "\]\s*$" >> /dev/null
 	if [ $? -eq 0 ];
 	then	
-		echo message
+		jsonArray=$(echo $1 | sed "s/^\s*\[//" | sed "s/\]\s*$//")
+		echo $jsonArray
+		counter=0
+		while [[ ${#jsonArray} > 0 ]];
+		do
+			echo $jsonArray | grep "^\s*{" >> /dev/null
+			if [ $? -eq 0 ];
+			then
+				brackets=0
+				firstBracket="false"
+				bracketIndex=0
+				for (( i=0; i < ${#jsonArray}; i=$((i+1)) ))
+				do
+					if [ "${jsonArray:$i:1}" == "{" ]; then
+						firstBracket="true"
+						brackets=$((brackets+1))
+					fi
+					if [ "${jsonArray:$i:1}" == '}' ]; then
+						brackets=$((brackets-1))
+					fi
+					if [ $firstBracket == 'true' ] && [ $brackets -eq 0 ];
+					then
+						bracketIndex=$i
+						break;
+					fi
+
+				done
+				jsonArrayElement=$(echo "${jsonArray:0:$((bracketIndex + 1))}")
+				echo $jsonArrayElement
+				newLength=${#jsonArray}
+				newLength=$((newLength-bracketIndex))
+				jsonArray=$(echo "${jsonArray:$((bracketIndex+1)):$newLength}")
+				jsonArray=$(echo $jsonArray | sed "s/^\s*,\s*//")
+				echo $jsonArray
+			else
+				echo "array"
+				echo $jsonArray | grep "^\s*\[" >> /dev/null
+				if [ $? -eq 0 ];
+				then
+					brackets=0
+					firstBracket="false"
+					bracketIndex=0
+					for (( i=0; i < ${#jsonArray}; i=$((i+1)) ))
+					do
+						if [ "${jsonArray:$i:1}" == "[" ]; then
+							firstBracket="true"
+							brackets=$((brackets+1))
+						fi
+						if [ "${jsonArray:$i:1}" == ']' ]; then
+							brackets=$((brackets-1))
+						fi
+						if [ $firstBracket == 'true' ] && [ $brackets -eq 0 ];
+						then
+							bracketIndex=$i
+							break;
+						fi
+
+					done
+
+					jsonArrayElement=$(echo "${jsonArray:0:$((bracketIndex + 1))}")
+					echo $jsonArrayElement
+					newLength=${#jsonArray}
+					newLength=$((newLength-bracketIndex))
+					jsonArray=$(echo "${jsonArray:$((bracketIndex+1)):$newLength}")
+					jsonArray=$(echo $jsonArray | sed "s/^\s*,\s*//")
+					echo $jsonArray
+
+				else
+					echo $jsonArray | grep "" >> /dev/null
+					if [ $? -eq 0 ];
+					then
+						kvPair=$(echo $jsonArray | sed "s/,.*$//g")
+						jsonObjectList=$(echo $jsonArray | sed "s/^\s*$kvPair//g")
+						jsonObjectList=$(echo $jsonArray | sed "s/^\w*,//g")
+					fi
+				fi
+			fi
+	
+	
+			#echo $kvPair
+			key=$(echo $kvPair | sed "s/:.*$//g")
+			value=$(echo $kvPair | sed "s/^[^:]*://g")
+			#echo "KEY: $key"
+			#echo "VALUE: $value"
+			if [ $2 -eq $counter ];
+			then	
+				shift
+				shift
+				arguments=($value $@)
+				./parseJSON.sh $arguments
+			fi
+			counter=$((counter+1))
+		done
+	
 		#json array
-	else
-		echo message
-		#json string
 	fi
 
 fi
