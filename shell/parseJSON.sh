feb0eebdf13d8670bb9d47a23c90d8ad9028b8fe
Jeremy Clifton <jeremyclif@outlook.com>
2019-01-06 09:13:31 -0500

    parseJson.sh

--- shell/parseJSON.sh
+++ shell/parseJSON.sh
@@ -2,11 +2,14 @@ if [ $# -eq 1 ]; then
 	echo $1
 	exit
 fi
+
 echo $1 | grep "^\s*{" | grep "}\s*$"  >> /dev/null
 if [ $? -eq 0 ];
 then
 	jsonObjectList=$(echo $1 | sed "s/^\s*{//g" | sed "s/}\s*$//")
-	while [[ ${#jsonObjectList} > 0 ]];
+	noMatch="true"
+	initialJsonObjectList=$jsonObjectList
+	while [ ${#jsonObjectList} -gt 0 ];
 	do
 		firstDoubleQuote="false"
 		quoteIndex=0
@@ -25,7 +28,6 @@ then
 		done
 
 		key=$(echo "${jsonObjectList:0:$((quoteIndex + 1))}")
-		
 	
 
 		nextLength=${#jsonObjectList}
@@ -144,8 +146,37 @@ then
 					nextLength=${#jsonObjectList}
 					nextLength=$((nextLength-quoteIndex))
 					jsonObjectList=$(echo "${jsonObjectList:$((quoteIndex+1)):$nextLength}")
-					jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*\:\s*//")
+					jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*,\s*//")
 
+				else
+					echo $jsonObjectList | grep "^\s*[0-9]\{1,\}\s*" >> /dev/null
+					if [ $? -eq 0 ];
+					then
+						value=$(echo $jsonObjectList | sed "s/,.*//g")
+						jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*$value\s*//g")
+						jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*,\s*//g")
+					else
+						echo $jsonObjectList | grep "^\s*true*\s*" >> /dev/null
+						if [ $? -eq 0 ]; then
+							value="true"
+							jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*true\s*,//g")
+						fi
+						echo $jsonObjectList | grep "^\s*false*\s*" >> /dev/null
+						if [ $? -eq 0 ]; then
+							value="false"
+							jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*false\s*,//g")
+						fi
+						echo $jsonObjectList | grep "^\s*null*\s*" >> /dev/null
+						if [ $? -eq 0 ]; then
+							value="null"
+							jsonObjectList=$(echo $jsonObjectList | sed "s/^\s*null\s*,//g")
+						fi
+
+					fi
+
+
+
+					#check for digit or boolean
 				fi
 			fi
 		fi
@@ -161,6 +192,11 @@ then
 		#echo "RETVAL: $value"	
 		if [ "\"$2\"" == "$key" ];
 		then	
+			noMatch="false"
+			if [ "$value" == "" ]; then
+				echo "error: json formatting error"
+				exit 1
+			fi
 			shift
 			shift
 			arguments=("$value $@")
@@ -168,13 +204,19 @@ then
 			exit
 		fi
 	done
+	if [ $noMatch == "true" ]; then
+		echo "error: no matching key: key $2 of object $initialJsonObjectList"
+		exit 1
+	fi
 else
 	echo $1 | grep "^\s*\[" | grep "\]\s*$" >> /dev/null
 	if [ $? -eq 0 ];
 	then	
-		jsonArray=$(echo $1 | sed "s/^\s*\[//" | sed "s/\]\s*$//")
+		
 		counter=0
-		while [[ ${#jsonArray} > 0 ]];
+		jsonArray=$(echo $1 | sed "s/^\s*\[//" | sed "s/\]\s*$//")
+		initialJsonArray=$jsonArray
+		while [ ${#jsonArray} -gt 0 ];
 		do
 			echo $jsonArray | grep "^\s*{" >> /dev/null
 			if [ $? -eq 0 ];
@@ -318,6 +360,11 @@ else
 
 			counter=$((counter+1))
 		done
+		counter=$((counter-1))
+		if [ $counter -lt $2 ]; then
+			echo "error: array index out of bounds: index $2 of array $initialJsonArray"
+			exit 1
+		fi
 	
 		#json array
 	fi
